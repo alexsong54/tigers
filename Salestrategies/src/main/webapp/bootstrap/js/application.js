a40dfc2a35be6f7a595ee5926d3cf5b9ae739037
Sam sun <sunjichao@live.com>
2014-01-20 14:01:59 +0800

    mini menubar

--- Salestrategies/src/main/webapp/bootstrap/js/application.js
+++ Salestrategies/src/main/webapp/bootstrap/js/application.js
@@ -47,8 +47,10 @@
             var offsetTop      = $sideBar.offset().top
             var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
             var navOuterHeight = $('.bs-docs-nav').height()
+            if (navOuterHeight==null){
+            	navOuterHeight = offsetTop - sideBarMargin
+          }
 
-//            alert(this.top = offsetTop - navOuterHeight - sideBarMargin)
            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
           }
         , bottom: function () {
