69834b6582006f36ffb811f6fff4360a4d2f5f52
DongJingChao <447958999@qq.com>
2014-03-14 10:28:42 +0800

    modify homepage style

--- Salestrategies/src/main/java/com/bxy/salestrategies/HomePage.java
+++ Salestrategies/src/main/java/com/bxy/salestrategies/HomePage.java
@@ -1,18 +1,25 @@
 package com.bxy.salestrategies;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 
 import com.bxy.salestrategies.common.Entity;
 import com.bxy.salestrategies.common.Field;
 import com.bxy.salestrategies.common.PageableTablePanel;
 import com.bxy.salestrategies.db.DAOImpl;
+import com.bxy.salestrategies.model.Choice;
 import com.bxy.salestrategies.util.Configuration;
+import com.google.common.collect.Maps;
 
 
 /**
@@ -22,41 +29,93 @@ import com.bxy.salestrategies.util.Configuration;
 public class HomePage extends Index implements AuthenticatedWebPage
 {  
 //	String serachInput = getRequest().getRequestParameters().getParameterValue("search_input").toString();
-	String relationTableName = "account";
-	final Map<String, Entity> entities = Configuration.getEntityTable();
-    final Entity entity = entities.get(relationTableName);
+    private final Map<String, IModel> models = Maps.newHashMap();
     String search_target;
     public HomePage(){
-    	 Form form = new Form("form"){
-    		 protected void onSubmit(){
-    			 List<Map> maplist = null;
-                 	String sql = "select * from account ";
-                 	 maplist = DAOImpl.queryEntityRelationList(sql);
-                 	 setResponsePage(new HomePage());
-    		 }
-    	 };
-        form.add(new TextField<String>("search_input", new PropertyModel<String>(this, "search_target")));  
+    	
+    	
+    	 Map<String, Entity> entities = Configuration.getEntityTable();
+         final Entity entity = entities.get("alert");
+        List<Entity> entitys = Configuration.getEntities();
+        IModel<Choice> selected_model =  new Model(new Choice(0l,"account"));
+    	 List<Choice> entityChoice = new ArrayList<Choice>();
+    	    final Map<String, Entity> entityList = new HashMap();
+    	    for(Entity entityName :entitys){
+    	    	if(entityName.isGlobalsearch()){
+    	    		Choice choice =new Choice();
+    	        	choice.setVal(entityName.getDisplay());
+    	        	choice.setName(entityName.getName());
+    	        	entityChoice.add(choice);
+    	            entityList.put(String.valueOf(entityName.getName()), entityName); 
+    	    	}
+    	    }
+    	TextField search_input = new TextField("search_input", new PropertyModel(this, "search_target"));
+        DropDownChoice search_select = new	DropDownChoice("search_select",selected_model,entityChoice, new IChoiceRenderer<Choice>() {
+            @Override
+            public Object getDisplayValue(Choice choice) {
+                // TODO Auto-generated method stub
+                return choice.getVal();
+            }
+            @Override
+            public String getIdValue(Choice choice, int index) {
+                // TODO Auto-generated method stub
+                return String.valueOf(choice.getName());
+            }
+            
+            
+        });
+        models.put("entityName", selected_model);
+        search_select.setNullValid(false);
+       
+        Form form = new Form("form")
+        {
+        	
+            @Override
+            protected void onSubmit()
+            {  
+            	String entityName = String.valueOf(((Choice) models.get("entityName").getObject()).getName());
+            	Entity  en =  entityList.get(entityName);
+                String sql   = en.getSql();
+                search_target = (search_target == null || search_target.equalsIgnoreCase("*")) ? "" : search_target;
+                
+                List<Field> searchableFields = en.getSearchableFields();
+                String joint = " like '%" + search_target + "%'";
+                String likequery = "";
+                for (Field sf : searchableFields)
+                {
+                    likequery = likequery + " OR " + sf.getName() + joint;
+                }
+                if(en.getName().equalsIgnoreCase("coaching")||en.getName().equalsIgnoreCase("activity")||en.getName().equalsIgnoreCase("willcoaching")){
+                	sql = sql + " where title like '%" + search_target + "%' " + likequery;
+                } else{
+                	sql = sql + " where name like '%" + search_target + "%' " + likequery;
+                }
+                System.out.println(sql);
+                List datalist = null;
+                
+                       
+                    	if(en.getName().equalsIgnoreCase("account")){
+                    		datalist = DAOImpl.queryEntityRelationList(sql);	
+                    	}else if(en.getName().equalsIgnoreCase("contact")){
+                    		datalist = DAOImpl.queryEntityRelationList(sql);
+                    	}else if(en.getName().equalsIgnoreCase("coaching")||en.getName().equalsIgnoreCase("activity")||en.getName().equalsIgnoreCase("willcoaching")){
+                    		datalist = DAOImpl.queryEntityRelationList(sql);
+                    	}
+                
+                if(entityName.equals("account")){
+                	setResponsePage(new AccountPage(datalist));
+                }else if(entityName.equals("contact")){
+                	setResponsePage(new ContactPage(datalist));
+                }else if(entityName.equals("activity")){
+                	setResponsePage(new ActivityPage(datalist));
+                }
+                
+            }
+            
+        };
+//        ChoiceRenderer choiceRenderer = new ChoiceRenderer<Entity>("name","URL");
+        
         add(form);
-        List list = new ArrayList<>();
-//		if (list == null) {
-            list=DAOImpl.queryEntityRelationList("Select * from account where 1=0 ");
-//        }
-		
-       add(new PageableTablePanel("datalist", entity, list, null));
-    }
-    private String assembleSearchingSQL( final Entity entity) {
-        String sql = entity.getSql();
-        
-        search_target = (search_target==null || search_target.equalsIgnoreCase("*"))? "":search_target;
-        
-        List<Field> searchableFields = entity.getSearchableFields();
-        String joint = " like '%"+search_target+"%'";
-        String likequery = "";
-        for(Field sf:searchableFields){
-            likequery = likequery + " OR "+ sf.getName() + joint;
-        }
-         sql =  sql + " where name like '%"+search_target+"%' " + likequery ;
-         System.out.println(sql);
-        return sql;
-    }
-}
+        form.add(search_select);
+        form.add(search_input);
+}}
