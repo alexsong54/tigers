c115fd891119b5a55e5313e88d16f27dc668c7be
DongJingChao <447958999@qq.com>
2014-03-17 14:44:45 +0800

    update sql

--- Salestrategies/src/db/update_12.sql
+++ Salestrategies/src/db/update_12.sql
@@ -3,3 +3,11 @@ CREATE TABLE `strategies`.`opportunity_probability_pl` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `val` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
+INSERT INTO `strategies`.`opportunity_probability_pl` (`val`) VALUES ('10%');
+INSERT INTO `strategies`.`opportunity_probability_pl` (`val`) VALUES ('20%');
+INSERT INTO `strategies`.`opportunity_probability_pl` (`val`) VALUES ('30%');
+INSERT INTO `strategies`.`opportunity_probability_pl` (`val`) VALUES ('50%');
+INSERT INTO `strategies`.`opportunity_probability_pl` (`val`) VALUES ('70%');
+INSERT INTO `strategies`.`opportunity_probability_pl` (`val`) VALUES ('90%');
+INSERT INTO `strategies`.`opportunity_probability_pl` (`val`) VALUES ('100%');
+INSERT INTO `strategies`.`opportunity_probability_pl` (`val`) VALUES ('0%');
